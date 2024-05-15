b395b16aceddb940350e28169b920ebfc603676d
SantiagoT81 <santytouris91@gmail.com>
2024-05-14 21:42:02 -0300

    first commit

---
+++ app/src/main/java/ar/edu/uade/c12024/tpo/UI/SplashActivity.kt
@@ -0,0 +1,35 @@
+package ar.edu.uade.c12024.tpo.UI
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ar.edu.uade.c12024.tpo.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Remover status bar superior durante splash.
+        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        //actionBar?.hide()
+
+
+        //Punto de cambio a pantalla LOGIN
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+}
